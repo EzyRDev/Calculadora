ef886528a74fe08e3585f6bf08e0c7bd3d884e85
EzyRDev <130946051+EzyRDev@users.noreply.github.com>
2023-05-12 15:04:21 -0300

    Update SimpleJavaServer.java

--- SimpleJavaServer.java
+++ SimpleJavaServer.java
@@ -1,31 +1,56 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
 public class SimpleJavaServer {
 
-public static void main(String[] args) {
-    try {
-        ServerSocket s = new ServerSocket(9999);
-        while (true) {
-            Socket c = s.accept();
-            DataInputStream in = new DataInputStream(c.getInputStream());
-            DataOutputStream out = new DataOutputStream(c.getOutputStream());
-            int sum = 0;
-            int num;
-            do {
-                num = in.readInt();
-                if (num != -1)
-                sum += num;
-            } while (num != -1);
-            out.writeInt(sum);
-            c.close();
+    public static void main(String[] args) {
+        try {
+            ServerSocket s = new ServerSocket(9999);
+            System.out.println("Servidor ouvindo na porta 9999...");
+            while (true) {
+                Socket c = s.accept();
+                System.out.println("Conexão estabelecida com " + c.getInetAddress());
+                InputStream i = c.getInputStream();
+                OutputStream o = c.getOutputStream();
+                String str;
+                byte[] buffer = new byte[1024];
+                byte[] line = new byte[1024];
+                i.read(line);
+                str = new String(line);
+                System.out.println("Comando recebido: " + str);
+                if (str.trim().equalsIgnoreCase("SOMA")) {
+                    int len = i.read(buffer);
+                    String input = new String(buffer, 0, len);
+                    String[] numbers = input.trim().split(" ");
+                    int sum = Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+                    o.write(("A soma é " + sum).getBytes());
+                }
+                if (str.trim().equalsIgnoreCase("SUB")) {
+                    int len = i.read(buffer);
+                    String input = new String(buffer, 0, len);
+                    String[] numbers = input.trim().split(" ");
+                    int sum = Integer.parseInt(numbers[0]) - Integer.parseInt(numbers[1]);
+                    o.write(("O resultado da subtração é " + sum).getBytes());
+                }
+                if (str.trim().equalsIgnoreCase("MULT")) {
+                    int len = i.read(buffer);
+                    String input = new String(buffer, 0, len);
+                    String[] numbers = input.trim().split(" ");
+                    int sum = Integer.parseInt(numbers[0]) * Integer.parseInt(numbers[1]);
+                    o.write(("O resultado da multiplição é " + sum).getBytes());
+                }
+                if (str.trim().equalsIgnoreCase("DIV")) {
+                    int len = i.read(buffer);
+                    String input = new String(buffer, 0, len);
+                    String[] numbers = input.trim().split(" ");
+                    int sum = Integer.parseInt(numbers[0]) / Integer.parseInt(numbers[1]);
+                    o.write(("O resultado da divisão é " + sum).getBytes());
+                }
+
+            }
+
+        } catch (Exception err) {
+            System.err.println(err);
         }
-    } catch (Exception err) {
-        System.err.println(err);
     }
 }
-}
-
-
